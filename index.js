38bd4f0ef118bd6261afd947771f2041e6c7b443
HoaAyWK <vycoi1511@gmail.com>
2021-08-12 01:09:06 -0700

    Using mongodb session store

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
+var MongoStore = require('connect-mongo');
 
 var productRoute = require('./routes/product.route');
 var userRoute = require('./routes/user.route');
@@ -13,8 +14,12 @@ var userRoute = require('./routes/user.route');
 var app = express();
 var port = 3000;
 
-mongoose.connect(process.env.MONGO_URL,
-    {useNewUrlParser: true, useUnifiedTopology: true});
+const clientPromise = mongoose.connect(
+    process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true})
+    .then(prm => prm.connection.getClient());
+
 require('./config/passport');
 
 app.set('view engine', 'pug');
@@ -23,14 +28,20 @@ app.set('views', './views');
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cookieParser());
-app.use(session({secret: process.env.SESSION_SECRET,
-    resave: false, saveUninitialized: false}));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ clientPromise: clientPromise }),
+    cookies: { maxAge: 120 * 60 * 1000 }
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
 app.use((req, res, next) => {
     res.locals.signin = req.isAuthenticated();
+    res.locals.session = req.session;
     next();
 });
 app.use('/', productRoute);
